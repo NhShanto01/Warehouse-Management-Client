591092afa09448c5dec99bd393321417f72e88f4
NhShanto01 <mdnurhossain940@gmail.com>
2022-05-09 23:25:40 +0600

    modified banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,7 +4,7 @@ import { Carousel } from 'react-bootstrap';
 const Banner = () => {
     return (
         <div className='container'>
-            <Carousel fade className=' fs-3'>
+            <Carousel>
 
                 <Carousel.Item>
                     <img
@@ -13,9 +13,9 @@ const Banner = () => {
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption className='text-info'>
+                    <Carousel.Caption className='text-warning'>
                         <h3>Sports-high-quality-goods</h3>
-                        <p>In addition to our high quality standards.Naturally, the highest level of safety as well as simple and quick handling of the equipment is of great importance to us</p>
+                        <p className='fw-bold'>In addition to our high quality standards.Naturally, the highest level of safety as well as simple and quick handling of the equipment is of great importance to us</p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -25,9 +25,9 @@ const Banner = () => {
                         src='https://i.ibb.co/vBL8fQd/soccer-ball-warehouse-600x400.png'
                         alt="First slide"
                     />
-                    <Carousel.Caption className='text-info'>
+                    <Carousel.Caption className='text-warning'>
                         <h3>Sports and health</h3>
-                        <p>Sports and health go hand in hand, offering people all over the world, of different abilities and ages, the chance for happier, healthier and more productive lives.</p>
+                        <p className='fw-bold'>Sports and health go hand in hand, offering people all over the world, of different abilities and ages, the chance for happier, healthier and more productive lives.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -38,9 +38,9 @@ const Banner = () => {
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption className='text-info'>
+                    <Carousel.Caption className='text-warning'>
                         <h3>Career-in-Sports</h3>
-                        <p>Just a privileged few athletes make it to the summit of playing professional sports. Luckily for job seekers searching for off-field careers in this field</p>
+                        <p className='fw-bold'>Just a privileged few athletes make it to the summit of playing professional sports. Luckily for job seekers searching for off-field careers in this field</p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -53,9 +53,9 @@ const Banner = () => {
                         alt="Fouth slide"
                     />
 
-                    <Carousel.Caption className='text-info'>
+                    <Carousel.Caption className='text-warning'>
                         <h3>Soccer-manufacturing</h3>
-                        <p>There are two main elements of the soccer ball. One is the inner bladder, the second is the outer covering. Sometimes the whole soccer ball is made under one roof.</p>
+                        <p className='fw-bold'>There are two main elements of the soccer ball. One is the inner bladder, the second is the outer covering. Sometimes the whole soccer ball is made under one roof.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
